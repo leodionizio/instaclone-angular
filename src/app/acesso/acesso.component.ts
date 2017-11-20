2f315df19c11cff38cb1c488cb6b0f39669e414d
Leonardo Dionizio <leodionizio13@gmail.com>
2017-11-20 12:02:59 -0200

    access component

---
+++ src/app/acesso/acesso.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, trigger, style, state, transition, animate } from '@angular/core';
+
+@Component({
+  selector: 'app-acesso',
+  templateUrl: './acesso.component.html',
+  styleUrls: ['./acesso.component.scss'],
+  animations: [
+    trigger('animation-banner', [
+      state('created', style({
+        opacity: 1
+      })),
+      transition('void => created', [
+        style({ opacity: 0, transform: 'translate(-70px, 0)' }),
+        animate('500ms 0s ease-in-out') // duração, delay e aceleração
+      ])
+    ]),
+    trigger('animation-panel', [
+      state('created', style({
+        opacity: 1
+      })),
+      transition('void => created', [
+        style({ opacity: 0, transform: 'translate(70px, 0)' }),
+        animate('500ms 0s ease-in-out') // duração, delay e aceleração
+      ])
+    ])
+  ]
+})
+export class AcessoComponent implements OnInit {
+
+  public stateBanner: string = 'created';
+  public statePanel: string = 'created';
+
+  public cadastro: boolean = false;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public exibirPainel(event: string): void {
+    this.cadastro = event === 'cadastro' ? true : false;
+  }
+}
